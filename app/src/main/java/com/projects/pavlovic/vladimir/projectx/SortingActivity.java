1c565bb34b7d4761ad78644ab2457b5887746a25
Vladimir Pavlovic <patjko@gmail.com>
2017-04-21 19:23:56 +0200

    Nastavak kuci
    nastavak kuci

--- app/src/main/java/com/projects/pavlovic/vladimir/projectx/SortingActivity.java
+++ app/src/main/java/com/projects/pavlovic/vladimir/projectx/SortingActivity.java
@@ -2,20 +2,12 @@ package com.projects.pavlovic.vladimir.projectx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class ChooseActivity extends AppCompatActivity {
+public class SortingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose);
-    }
-
-    public void startSorting(View view) {
-        
-    }
-
-    public void startBetting(View view) {
+        setContentView(R.layout.activity_sorting);
     }
 }
